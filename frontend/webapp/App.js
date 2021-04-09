65a20caa1e71b2947e4b69ae54f32a3dbe3e0b80
meghaMS <meghana.suresh@tarento.com>
2021-04-09 18:12:02 +0530

    Clear cache changes

--- frontend/webapp/App.js
+++ frontend/webapp/App.js
@@ -2,14 +2,36 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import storeFactory from "./src/flux/store/store";
 import AppRoutes from './src/mobile.routes';
-
+import ClearCache from 'react-clear-cache';
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={storeFactory}>
-        <AppRoutes />
-      </Provider>
+      <div>
+        <ClearCache>
+          {({ isLatestVersion, emptyCacheStorage }) => (
+            <div>
+              {!isLatestVersion && (
+                <p>
+                  <a
+                    href="#"
+                    onClick={e => {
+                      e.preventDefault();
+                      emptyCacheStorage();
+                    }}
+                  >
+                    Update version
+                </a>
+                </p>
+              )}
+              <Provider store={storeFactory}>
+                <AppRoutes />
+              </Provider>
+            </div>
+          )}
+        </ClearCache>
+
+      </div>
     );
   }
 }
