d0592c2f674860d98c4fd1cbc0825aa4fbf8a88e
meghaMS <meghana.suresh@tarento.com>
2021-04-21 11:20:01 +0530

    Reverted

--- frontend/webapp/App.js
+++ frontend/webapp/App.js
@@ -1,63 +1,9 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Provider } from "react-redux";
 import storeFactory from "./src/flux/store/store";
 import AppRoutes from './src/mobile.routes';
-import ClearCache from 'react-clear-cache';
-import { withSnackbar } from "notistack";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newVersionAvailable: false,
-      waitingWorker: {},
-    };
-  }
-
-  onServiceWorkerUpdate = (registration) => {
-    this.setState({
-      waitingWorker: registration && registration.waiting,
-      newVersionAvailable: true,
-    });
-  };
-
-  updateServiceWorker = () => {
-    const { waitingWorker } = this.state;
-    waitingWorker && waitingWorker.postMessage({ type: "SKIP_WAITING" });
-    this.setState({ newVersionAvailable: false });
-    window.location.reload();
-  };
-
-  refreshAction = (key) => { //render the snackbar button
-    return (
-      <Fragment>
-        <Button
-          className="snackbar-button"
-          size="small"
-          onClick={this.updateServiceWorker}
-        >
-          {"refresh"}
-        </Button>
-      </Fragment>
-    );
-  };
-
-
-  componentDidMount = () => {
-    const { enqueueSnackbar } = this.props;
-    const { newVersionAvailable } = this.state;
-if (process.env.NODE_ENV === 'production') {
-    serviceWorker.register({ onUpdate: this.onServiceWorkerUpdate });
-}
-
-    if (newVersionAvailable) //show snackbar with refresh button
-      enqueueSnackbar("A new version was released", {
-        persist: true,
-        variant: "success",
-        action: this.refreshAction(),
-      });
-  };
-
+export default class App extends Component {
   render() {
     return (
               <Provider store={storeFactory}>
@@ -67,5 +13,3 @@ if (process.env.NODE_ENV === 'production') {
   }
 }
 
-
-export default withSnackbar(App)
